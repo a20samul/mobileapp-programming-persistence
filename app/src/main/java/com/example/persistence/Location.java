279071cf445cc5768c63065b73f3d6cd7d4f2f6b
a20samul <a20samul@student.his.se>
2021-05-22 16:54:36 +0200

    Edited layout

--- app/src/main/java/com/example/persistence/Location.java
+++ app/src/main/java/com/example/persistence/Location.java
@@ -12,6 +12,6 @@ public class Location {
     }
 
     @Override
-    public String toString() { return city + " " + country + " " + population;}
+    public String toString() { return city + ", " + country + ", " + population;}
 
 }
